909b368a58bf64ce424bd51d4fdab8326fe22baf
Aleksei Shaikhaleev <nimdraug.sael@gmail.com>
2022-11-08 14:52:35 +0800

    fix dvc config

--- test_live_metrics.py
+++ test_live_metrics.py
@@ -11,8 +11,8 @@ live = Live()
 time.sleep(5)
 
 for i in range(params["epochs"]):
-    live.log("foo", i + random.random())
-    live.log("bar", i + random.random())
+    live.log_metric("foo", i + random.random())
+    live.log_metric("bar", i + random.random())
     live.next_step()
 
     time.sleep(6)
